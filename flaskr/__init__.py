3b57e25275c88f402e87bdcb847769b06f681e49
Yuto Urashima <yuto.urashima24@gmail.com>
2023-08-28 13:58:32 +0900

    アプリケーションの用意（Application Setup）

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # Flaskインスタンスを作成
+    app = Flask(__name__, instance_relative_config=True)
+
+    # appが使用する標準設定
+    app.config.from_mapping(
+        SECRET_KEY = "dev",
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    # config.pyがある場合、標準設定を上書き
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    # app.instance_pathが確実に存在するようにする
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "hello world"
+    
+    return app
